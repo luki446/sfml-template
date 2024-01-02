7e20cc3a357bc9da5d8be6bcd66b5ffd28824896
Luki446 <luki446@gmail.com>
2024-01-02 15:41:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SFML CMake example project
+
+SFML template project created to symplify process of setting up developemnt environment.
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <SFML/Window.hpp>
+
+int main() {
+  sf::Window window(sf::VideoMode(800, 600), "Example window");
+
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+  }
+
+  return 0;
+}
